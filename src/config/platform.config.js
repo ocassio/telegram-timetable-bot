cd271a20643d281ec9bf2a88196ed72e7f6b414b
Alexander Ionov <orlicus@gmail.com>
2017-08-31 17:06:03 +0400

    Draft actions handling

---
+++ src/config/platform.config.js
@@ -0,0 +1,6 @@
+const platformUrl = process.env.TIMETABLE_BOT_PLATFORM_URL
+
+module.exports = {
+    platformUrl,
+    actionsUrl: `${platfromUrl}/api/actions`
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const bb = require('bot-brother')
-const config = require('./config')
+const axios = require('axios')
 
-const bot = bb(config)
+const botConfig = require('./config/bot.config')
+const platformConfig = require('./config/platform.config')
 
+const bot = bb(botConfig)
+
+const actionsResponse = await axios.get(platformConfig.actionsUrl)
+
+actionsResponse.data.forEach(action => {
+    bot.command(action).invoke((ctx) => {
+        const userId = `telegram:${ctx.meta.user.id}`
+        const response = await axios.get(`${platformConfig.actionsUrl}/${action}?userId=${userId}`)
+        sendMessage(ctx, response.data)
+    })
+})
+
+function sendMessage(ctx, meta) {
+    const message = `${meta.title}\n\n${meta.message}`
+    ctx.sendMessage(message)
+
+    if (meta.actions) {
+        const keyboard = meta.actions.map(action => {}) //TODO: create actual keyboard
+        ctx.keyboard(keyboard)
+    }
+}
