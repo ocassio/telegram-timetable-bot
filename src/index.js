4f428c1340539c8efe0f092400f8e93b86ff8065
ocassio <orlicus@gmail.com>
2017-09-02 16:30:39 +0400

    Handling response action

--- src/index.js
+++ src/index.js
@@ -8,26 +8,53 @@ const bot = bb(botConfig)
 
 bot.command(/.*/).invoke(ctx => execute(ctx, ctx.command.name))
 
-bot.api.onText(/^[^\/].*$/, msg => {
-    bot.api.sendMessage(msg.chat.id, 'Not a command')
+bot.api.onText(/^[^\/].*$/, async msg => {
+    const userId = getUserId(msg.from.id)
+    const response = await axios.get(`${platformConfig.actionsUrl}/response?userId=${userId}`)
+    
+    const meta = response.data
+    const messages = meta.messages
+    const keyboard = getKeyboard(meta)
+
+    for (let i = 0; i < messages.length - 1; i++) {
+        bot.api.sendMessage(msg.chat.id, messages[i])
+    }
+
+    const lastMessage = messages[messages.length - 1]
+    if (lastMessage) {
+        bot.api.sendMessage(msg.chat.id, lastMessage, {
+            'reply_markup': keyboard
+        })
+    }
 })
 
 async function execute(ctx, action) {
-    const userId = `telegram:${ctx.meta.user.id}`
+    const userId = getUserId(ctx.meta.user.id)
     const response = await axios.get(`${platformConfig.actionsUrl}/${action}?userId=${userId}`)
     sendMessage(ctx, response.data)
 }
 
 function sendMessage(ctx, meta) {
-    if (meta.buttons) {
-        const keyboard = meta.buttons.map(button => {
-            const key = {}
-            key[button.label] = { go: button.action }
-            return [key]
-        })
+    const keyboard = getKeyboard(meta)
+    if (keyboard) {
         ctx.keyboard(keyboard)
     } else {
         ctx.hideKeyboard()
     }
     meta.messages.forEach(message => ctx.sendMessage(message))
+}
+
+function getUserId(id) {
+    return `telegram:${id}`
+}
+
+function getKeyboard(meta) {
+    if (!meta.buttons || meta.buttons.length === 0) {
+        return
+    }
+    return meta.buttons.map(button => {
+        const key = {}
+        key[button.label] = { go: button.action }
+        return [key]
+    })
 }
